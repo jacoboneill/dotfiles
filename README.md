f9387881e4a4ddfe6b52e8454a37f507da7d577c
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-07 20:03:52 +0000

    Add Pane Navigation and Window Management Keymaps

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Video
-Up to: https://www.youtube.com/watch?v=ZjMzBd1Dqz8&t=3600s
+Up to:
+    https://www.youtube.com/watch?v=ZjMzBd1Dqz8&t=3726s
 
 # TODO
 - [x] Add \<leader\>n for number toggle
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,18 @@
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 -- Directory Navigation
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- Number Toggle
-keymap.set("n", "<leader>n", ":set relativenumber!<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>n", ":set relativenumber!<CR>", opts)
 
+-- Pane Navigation
+keymap.set("n", "<C-h>", "C-w>h", opts) -- Navigate Left
+keymap.set("n", "<C-j>", "C-w>j", opts) -- Navigate Down
+keymap.set("n", "<C-k>", "C-w>k", opts) -- Navigate Up
+keymap.set("n", "<C-l>", "C-w>l", opts) -- Navigate Right 
+
+-- Window Management
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
+keymap.set("n", "<leader>sh", ":hsplit<CR>", opts) -- Split Horizontally 
