5eb6360ef41487c0ccfeef6e21822ac7f86e96c4
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-09 18:53:10 +0000

    Add Telescope.nvim

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+local mapkey = require("util.keymapper").mapkey
+
+local config = function()
+	local telescope = require("telescope")
+	telescope.setup({
+		defaults = {
+			mappings = {
+				i = {
+					["<C-j>"] = "move_selection_next",
+					["<C-k>"] = "move_selection_previous",
+          ["<C-q>"] = "close",
+				},
+			},
+		},
+	})
+end
+
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.3",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = config,
+	keys = {
+		mapkey("<leader>fk", "Telescope keymaps", "n"),
+		mapkey("<leader>fh", "Telescope help_tags", "n"),
+		mapkey("<leader>ff", "Telescope find_files", "n"),
+		mapkey("<leader>fg", "Telescope live_grep", "n"),
+		mapkey("<leader>fb", "Telescope buffers", "n"),
+	},
+}
