93254fe62b4063317ffe42a5e6397d10807eb211
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-09 18:55:00 +0000

    Add Comment.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,11 +8,15 @@ keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 keymap.set("n", "<leader>n", ":set relativenumber!<CR>", opts)
 
 -- Pane Navigation
-keymap.set("n", "<C-h>", "C-w>h", opts) -- Navigate Left
-keymap.set("n", "<C-j>", "C-w>j", opts) -- Navigate Down
-keymap.set("n", "<C-k>", "C-w>k", opts) -- Navigate Up
-keymap.set("n", "<C-l>", "C-w>l", opts) -- Navigate Right 
+keymap.set("n", "<C-h>", "<C-w>h", opts) -- Navigate Left
+keymap.set("n", "<C-j>", "<C-w>j", opts) -- Navigate Down
+keymap.set("n", "<C-k>", "<C-w>k", opts) -- Navigate Up
+keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right 
 
 -- Window Management
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
 keymap.set("n", "<leader>sh", ":hsplit<CR>", opts) -- Split Horizontally 
+
+-- Comments
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", {noremap = false})
+vim.api.nvim_set_keymap("v", "<C-_>", "gcc", {noremap = false})
