f991d8267b020d6b7a25d5895cd6fa73cb2b32a4
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-24 18:26:34 +0000

    Add File Browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,14 @@
 local mapkey = require("util.keymapper").mapkey
 
+local telescope_file_browser = {
+  "nvim-telescope/telescope-file-browser.nvim",
+  lazy = false,
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+}
+
 local config = function()
 	local telescope = require("telescope")
+  local file_browser = telescope.extensions.file_browser
 	telescope.setup({
 		defaults = {
 			mappings = {
@@ -10,12 +17,18 @@ local config = function()
 					["<C-k>"] = "move_selection_previous",
           ["<C-q>"] = "close",
 				},
+        n = {
+          ["q"] = "close",
+          ["a"] = file_browser.actions.create
+        },
 			},
 		},
 	})
+
+  telescope.load_extension "file_browser"
 end
 
-return {
+local telescope = {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.3",
 	lazy = false,
@@ -26,6 +39,12 @@ return {
 		mapkey("<leader>fh", "Telescope help_tags", "n"),
 		mapkey("<leader>ff", "Telescope find_files", "n"),
 		mapkey("<leader>fg", "Telescope live_grep", "n"),
-		mapkey("<leader>fb", "Telescope buffers", "n"),
+		mapkey("<leader>fx", "Telescope buffers", "n"),
+    mapkey("<leader>fb", "Telescope file_browser", "n"),
 	},
+}
+
+return {
+  telescope,
+  telescope_file_browser,
 }
