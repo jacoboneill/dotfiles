4b482307142aac2e3a1801b995bcb2e4dcd8959f
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-26 18:22:01 +0000

    Add .oh-my-zsh plugins and themes

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,7 +35,7 @@ brew uninstall --cask dockutil;
 brew autoremove;
 
 # Install ZSH Themes and Plugins
-zsh
+ZSH_CUSTOM="$HOME/.config/zsh/.oh-my-zsh";
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
