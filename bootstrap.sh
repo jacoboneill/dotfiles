5d1484d23cc8a2fd6c05c31576e1cff822d2f16f
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-26 18:08:15 +0000

    Add ZSH Plugin Installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,4 +34,10 @@ dockutil -a ~/Downloads/ --display stack --restart;
 brew uninstall --cask dockutil;
 brew autoremove;
 
+# Install ZSH Themes and Plugins
+zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Restart ZSH
 zsh
